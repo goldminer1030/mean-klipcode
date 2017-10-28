14dc491c16c287da1e8b62250347a349b72957d7
Paul Brauner <paul_brauner@outlook.com>
2017-10-28 04:04:15 +0200

    trying to use static folder

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 var express = require('express'); //
 var app = express(); //
 
-app.get('/',function (req,res) {
- res.send('Hello World!');
-});
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, function(){
  console.log('Server On!');
